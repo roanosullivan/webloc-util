1bafb977cca170f93efdd4b0f6dc018416a957fe
Roan OSullivan <roanosullivan@sosuke.729x.com>
2013-07-18 10:53:35 -0400

    == Generalized XML select functions ==
     * Fixed and documented assumptions
     * Extracted generic XML select functions where possible
     * Grouped PLIST-specific XML select functions
     * Added some "test" functions that demo using enliven to select text from XML

--- src/webloc_util/core.clj
+++ src/webloc_util/core.clj
@@ -2,22 +2,38 @@
   [:require [net.cgrand.enlive-html :as html]]
   (:gen-class))
 
-(defn has-text [s]
-  (html/pred #(= (html/text %) s)))
+;; GENERIC XML select functions
 
-(defn get-entries-for-key
-  "Gets sequence of 'dict' parent elements where text value of child 'key' element = k"
-  [plist k]
-  (html/select plist [:plist [:dict (html/has [(has-text k)])]]))
+(defn element-that-has-child-with-text
+  "Returns selector for element e that has child c where text = t"
+  [e c t]
+  [e (html/has [c]) (html/has [(html/pred #(= (html/text %) t))])])
 
-(defn get-value
-  [dict]
-  (let [string-el (html/select dict [:string])]
-    (map html/text string-el)))
+(defn select-text
+  "Returns seq of strings representing text values of nodes matched by selector 'sel'"
+  [node-or-nodes sel]
+  (let [child-node (html/select node-or-nodes [sel])]
+    (map html/text child-node)))
 
-(defn get-url
+;; PLIST-specific XML select functions
+
+(defn select-plist-dict-entries-by-key
+  "Returns sequence of 'dict' parent nodes where (text of 'dict/key' node) == key-text"
+  [plist key-text]
+  (html/select plist [:plist (element-that-has-child-with-text :dict :key key-text)]))
+
+(defn select-url
+  "Selects value of URL entry from plist (if exists). plist must be an html-snippet or be able to be parsed as an html-snippet (e.g. XML string)"
   [plist]
-  (get-value (first (get-entries-for-key plist "URL"))))
+  (if (seq? plist)
+    ; if plist a seq assume has been parsed by html/html-snippet
+    (let [dict-node (first (select-plist-dict-entries-by-key plist "URL"))]
+      (select-text dict-node :string))
+    ; if plist not a seq, assume it's a string and attempt to parse
+    (do
+      (recur (html/html-snippet plist)))))
+
+;; ENTRY POINT
 
 (defn -main
   "I don't do a whole lot ... yet."
@@ -33,10 +49,21 @@
 <key>URL</key>
 <string>http://google.com</string>
 </dict>
+<dict>
+<yek>URL</yek>
+<string>http://google.com/yek</string>
+</dict>
+<dict>
+<key>LRU</key>
+<string>http://google.com/LRU</string>
+</dict>
 <coocoo>
 <key>URL</key>
 </string>http://coocoo</string>
 </coocoo>
+<random>3.14</random>
 </plist>")
 
-(def sample-snippet (html/html-snippet sample))
+(defn test1 [] (html/select (html/html-snippet sample) [[:dict (html/has [:key]) (html/has [(html/pred #(= (html/text %) "LRU"))])]]))
+
+(defn test2 [] (select-text (test1) :string))
