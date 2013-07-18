8a881dd9103c2626cbb5dab8cb92a4b5c1311050
Roan OSullivan <roanosullivan@sosuke.729x.com>
2013-07-18 13:18:26 -0400

    == Implemented HTML and MD Transform Functions =
     * Implemented two functions that take sequences of urls and sequences of links and transform them …
        * to-html
        * to-markdown

--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [enlive "1.1.1"]]
+                 [enlive "1.1.1"]
+                 [me.raynes/fs "1.4.4"]]
   :main webloc-util.core)
--- src/webloc_util/core.clj
+++ src/webloc_util/core.clj
@@ -1,7 +1,11 @@
 (ns webloc-util.core
-  [:require [net.cgrand.enlive-html :as html]]
+  [:require
+   [net.cgrand.enlive-html :as html]
+   [clojure.java.io :as io]
+   [me.raynes.fs :as fs]]
   (:gen-class))
 
+;; ------------------------------------
 ;; GENERIC XML select functions
 
 (defn element-that-has-child-with-text
@@ -15,6 +19,7 @@
   (let [child-node (html/select node-or-nodes [sel])]
     (map html/text child-node)))
 
+;; ------------------------------------
 ;; PLIST-specific XML select functions
 
 (defn select-plist-dict-entries-by-key
@@ -28,21 +33,74 @@
   (if (seq? plist)
     ; if plist a seq assume has been parsed by html/html-snippet
     (let [dict-node (first (select-plist-dict-entries-by-key plist "URL"))]
-      (select-text dict-node :string))
+      (apply str (select-text dict-node :string)))
     ; if plist not a seq, assume it's a string and attempt to parse
     (do
       (recur (html/html-snippet plist)))))
 
+;; ------------------------------------
+;; TRANSFORM
+
+(defn render
+  "Maybe something like this exists in net.cgrand.enlive-html, but I could not find it."
+  [node-or-nodes]
+  (apply str (html/emit* node-or-nodes)))
+
+(defn to-html-anchor
+  [url link]
+  (let [template (html/html-snippet "<a href=\"HREF\">LINK</a>")]
+    (html/transform
+     template [:a]
+     (html/do->
+      (html/set-attr :href url)
+      (html/content link)))))
+
+(defn to-html
+  [urls links]
+  (let [template (html/html-snippet "<ul><li>ANCHOR</li></ul>")
+        pairs (map #(vector %1 %2) urls links)]
+    (html/transform
+     template [:li]
+     (html/clone-for
+      [pair pairs]
+      (let [url (get pair 0)
+            link (get pair 1)]
+        (html/content (to-html-anchor url link)))))))
+
+(defn to-markdown-link
+  [url link]
+  (let [template "[LINK](URL)"]
+    (-> template
+        (clojure.string/replace #"URL" url)
+        (clojure.string/replace #"LINK" link))))
+
+(defn to-markdown
+  [urls links]
+  (let [pairs (map #(vector %1 %2) urls links)]
+    (reduce
+     #(let [url (get %2 0)
+            link (get %2 1)]
+        (str %1 " * " (to-markdown-link url link) "\n"))
+     "" pairs)))
+
+;; ------------------------------------
 ;; ENTRY POINT
 
 (defn -main
-  "I don't do a whole lot ... yet."
-  [name & args]
-  (println (str "Hello, " name "!")))
+  "Read plist from file and output html equivalent."
+  [op path & args]
+  (let [raw-xml (slurp path)
+        fname (.getName (io/file path))
+        bname (fs/base-name fname true)
+        url (select-url raw-xml)]
+    (if (= "html" op)
+      (to-html url bname)
+      (to-markdown url bname))))
 
+;; ------------------------------------
 ;; TEST SUPPORT
 
-(def sample "<?xml version=\"1.0\" encoding=\"UTF-8 \"?>
+(def sample-plist "<?xml version=\"1.0\" encoding=\"UTF-8 \"?>
 <!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN \" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd \">
 <plist version=\"1.0 \">
 <dict>
@@ -64,6 +122,14 @@
 <random>3.14</random>
 </plist>")
 
-(defn test1 [] (html/select (html/html-snippet sample) [[:dict (html/has [:key]) (html/has [(html/pred #(= (html/text %) "LRU"))])]]))
+(def sample-urls '("http://google.com" "http://yahoo.com"))
+
+(def sample-links '("GOOG" "YAHOO"))
+
+(defn test1 [] (html/select (html/html-snippet sample-plist) [[:dict (html/has [:key]) (html/has [(html/pred #(= (html/text %) "LRU"))])]]))
 
 (defn test2 [] (select-text (test1) :string))
+
+(defn test3 [] (println (render (to-html sample-urls sample-links))))
+
+(defn test4 [] (println (to-markdown sample-urls sample-links)))
